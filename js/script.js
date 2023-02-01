210acc57282e53e60fa589224ef2b54fb97fa279
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2023-02-01 19:13:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const dolar = document.querySelector('.dolar');
-var cotation = 5.06;
+var cotacao = 5.06;
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     const inputDolar = dolar.value;
 
-    var real = (cotation * inputDolar).toFixed(2);
+    var real = (cotacao * inputDolar).toFixed(2);
 
     var resultado = document.getElementById('resultado');
     resultado.innerHTML = '$' + inputDolar + ' equivalem a R$' + real;
